1a8baeae777a0e735ed5316c7a946c22532d0996
Jarrod Moyer <jarrodw9@gmail.com>
2020-01-30 14:03:11 -0600

    Menu Cloud Scrolling
    Added 4 different clouds to spawn at spesific locations, after which, when they reach a collider 2d past the screen, the cloud object will automatically destroy. Done using invokeRepeating on the cloudSpawn function

---
+++ UnityProject/Assets/Scripts/Menu/CloudCollider.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudCollider : MonoBehaviour
+{
+
+    public GameObject collidedCloud;
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "cloud")
+        {
+            collidedCloud = collision.gameObject;
+        }
+    }
+}
---
+++ UnityProject/Assets/Scripts/Menu/MainMenuManager.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuManager : MonoBehaviour
+{
+    
+    void Start()
+    {
+        InvokeRepeating("spawnCloud",0,cloudSpeed);
+    }
+
+    void Update()
+    {
+        moveCloud();
+    }
+
+    public void playButton()
+    {
+        //change scene to the scene indexed after this scene
+        Debug.Log("Changing Scene");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void quitButton()
+    {
+        Debug.Log("QUITTING");
+        Application.Quit();
+    }
+
+
+
+
+
+    public float cloudSpeed = 10f;
+    public GameObject[] cloudSpawns;
+    public Sprite[] cloudSprites;
+    public GameObject cloudPrefab;
+    public GameObject cloudCollider;
+    private Dictionary<GameObject, Transform> cloudDic = new Dictionary<GameObject, Transform>();
+    private int cloudNum = 0;
+    public void spawnCloud()
+    {
+        if (cloudNum == 0)
+        {
+            GameObject newCloud = Instantiate(cloudPrefab,cloudSpawns[1].transform);
+            newCloud.GetComponent<Image>().sprite = cloudSprites[0];
+            cloudDic.Add(newCloud,cloudSpawns[1].transform);
+            cloudNum += 1;
+        }
+        else if (cloudNum == 1)
+        {
+            GameObject newCloud = Instantiate(cloudPrefab, cloudSpawns[0].transform);
+            newCloud.GetComponent<Image>().sprite = cloudSprites[1];
+            cloudDic.Add(newCloud, cloudSpawns[0].transform);
+            cloudNum += 1;
+        }
+        else if (cloudNum == 2)
+        {
+            GameObject newCloud = Instantiate(cloudPrefab, cloudSpawns[1].transform);
+            newCloud.GetComponent<Image>().sprite = cloudSprites[3];
+            cloudDic.Add(newCloud, cloudSpawns[1].transform);
+            cloudNum += 1;
+        }
+        else if (cloudNum == 3)
+        {
+            GameObject newCloud = Instantiate(cloudPrefab, cloudSpawns[0].transform);
+            newCloud.GetComponent<Image>().sprite = cloudSprites[2];
+            cloudDic.Add(newCloud, cloudSpawns[0].transform);
+            cloudNum = 0;
+        }
+    }
+
+    public void moveCloud()
+    {
+        foreach (KeyValuePair<GameObject,Transform> p in cloudDic)
+        {
+            Vector3 movement = new Vector3(p.Value.position.x,0f,0f);
+            p.Key.transform.position -= movement * Time.deltaTime / cloudSpeed;
+        }
+
+        if (cloudCollider.GetComponent<CloudCollider>().collidedCloud != null)
+        {
+            cloudDic.Remove(cloudCollider.GetComponent<CloudCollider>().collidedCloud);
+            Destroy(cloudCollider.GetComponent<CloudCollider>().collidedCloud);
+        }
+    }
+}
